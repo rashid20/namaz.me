ed9050480c519bdfd9e7032506501809f0db4510
Ahmad Mushtaq <ahmad.mushtaq@gmail.com>
2012-08-18 20:58:34 +0300

    Testing the GithoobHook

--- updated.php
+++ updated.php
@@ -1,11 +1,12 @@
 <?php
+// this file is not used any more, GitHubHook/ folder is now used instead
 // author: Ahmad Mushtaq <ahmad.mushtaq@gmail.com>
 // github will call this file, when a commit is made to the namaz.me repository on github.
 // read https://help.github.com/articles/post-receive-hooks for more info.
 
-$payload=$_POST["payload"];
+// $payload=$_POST["payload"];
 
 // if commit to the master branch, just call git pull.
 // if commit was to th branch production, go one directory up and then call git pull there.
-system("git pull");
+// system("git pull");
 ?>
