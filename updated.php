31c30ef995cc8779268a774332fd3c9b74076b53
Ahmad Mushtaq <ahmad.mushtaq@gmail.com>
2012-08-18 20:42:01 +0300

    updated the github hook.

--- updated.php
+++ updated.php
@@ -3,6 +3,9 @@
 // github will call this file, when a commit is made to the namaz.me repository on github.
 // read https://help.github.com/articles/post-receive-hooks for more info.
 
-// we dont care about the post payload we get from github.
+$payload=$_POST["payload"];
+
+// if commit to the master branch, just call git pull.
+// if commit was to th branch production, go one directory up and then call git pull there.
 system("git pull");
 ?>
